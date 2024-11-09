9151aee17ed12e68b0d02fc43a03fc9647e3dafb
puftii <ivan35923@gmail.com>
2024-11-10 02:12:47 +0500

    Added prefabs for ability effects

--- Scripts/ScriptableObjects/Abilities.cs
+++ Scripts/ScriptableObjects/Abilities.cs
@@ -7,6 +7,11 @@ public class Ability : ScriptableObject
     public float cooldownTime;
     public float activeTime;
     public float staminaCost;
+    public LayerMask enemyLayers;
+    public GameObject AbilityPrefab;
+    public Vector3 positionOffset;
+    public Vector3 rotationOffset;
+
 
     public virtual void Activate(GameObject parent) {}
     public virtual void BeginCooldown(GameObject parent) { }
--- Scripts/ScriptableObjects/TPToEnemy.cs
+++ Scripts/ScriptableObjects/TPToEnemy.cs
@@ -8,8 +8,18 @@ public class TPToEnemy : Ability
 {
     public override void Activate(GameObject parent)
     {
-        ThirdPersonController _controller = parent.GetComponent<ThirdPersonController>();
-        _controller.TPToNearestEnemy();
+        Collider[] enemies = Physics.OverlapSphere(parent.transform.position, 15f, enemyLayers);
+
+        if (enemies.Length == 0 ) 
+        {
+            Transform enemy = enemies[0].transform;
+            GameObject poof = Instantiate(AbilityPrefab, parent.transform.position, Quaternion.Euler(Vector3.zero));
+            parent.transform.position = enemy.position + (enemy.position - parent.transform.position) / Vector3.Distance(enemy.position, parent.transform.position);
+            parent.transform.position = new Vector3(parent.transform.position.x, enemy.position.y, parent.transform.position.z);
+            parent.transform.LookAt(enemy.position);
+            parent.transform.rotation = Quaternion.Euler(0, parent.transform.rotation.y, 0);
+        }
+        
         //parent.transform.position = Vector3.zero;
 
     }
